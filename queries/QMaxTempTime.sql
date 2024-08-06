246603facfc5c34c4ac1bc3c21aaef67add94dd9
Ronan Bottoms <rpbottoms@gmail.com>
2024-08-06 15:10:39 -0700

    Updates to analysis

--- queries/QMaxTempTime.sql
+++ queries/QMaxTempTime.sql
@@ -1,13 +1,14 @@
 With MaxTempTimes AS (
         WITH RankedTemp AS (
-                SELECT DATE(DateTime) as Date, TIME(DateTime) as Time, Temperature, RANK() OVER (PARTITION BY DATE(DateTime) ORDER BY Temperature DESC) as Rank
-                FROM SEA_weather
+            SELECT DATE(DateTime) as Date, TIME(DateTime) as Time, 
+                Temperature, RANK() OVER (PARTITION BY DATE(DateTime) ORDER BY Temperature DESC) as Rank
+            FROM SEA_weather
         )
         SELECT Date, TIME(AVG((STRFTIME('%H', Time) * 60 + STRFTIME('%M', Time)) * 60), 'unixepoch') AS AvgTime
         FROM RankedTemp
         WHERE Rank = 1
         GROUP BY Date
 )
-SELECT Count(AvgTime)
+SELECT COUNT(Date), CAST(STRFTIME('%H', AvgTime) AS INT) as Hour
 FROM MaxTempTimes
-GROUP BY STRFTIME('%H', AvgTime)
+GROUP BY Hour
